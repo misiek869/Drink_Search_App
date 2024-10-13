ccaf8e0cbb4eb2b03ece9b6f58cc99859b124061
Michal Gawik <gawlik.zator@o2.pl>
2024-10-13 18:20:09 +0200

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+	{ path: '/', element: <h2>Home Page</h2> },
+	{
+		path: '/about',
+		element: <h2>About Page</h2>,
+	},
+])
 
 function App() {
 	const [count, setCount] = useState(0)
 
-	return <></>
+	return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App
