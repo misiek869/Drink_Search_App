c9feb604aa119fa6b981c55c2b65ff22c58a9a85
Michal Gawik <gawlik.zator@o2.pl>
2024-10-13 18:34:30 +0200

    setup links

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
-	return <div></div>
+	return (
+		<div>
+			<h1>About</h1>
+			<Link to='/'>Home</Link>
+		</div>
+	)
 }
 
 export default About
--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import About from './About'
 
 const HomeLayout = () => {
-	return <div></div>
+	return (
+		<div>
+			<h1>Home</h1>
+			<Link to='/about'>About</Link>
+		</div>
+	)
 }
 
 export default HomeLayout
