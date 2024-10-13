3c8984123d0510bea567286209e3007d944a95a9
Michal Gawik <gawlik.zator@o2.pl>
2024-10-13 18:29:31 +0200

    create exports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
-import React from 'react'
-
-const index = () => {
-	return <div></div>
-}
-
-export default index
+export { default as Landing } from './Landing'
+export { default as About } from './About'
+export { default as Cocktail } from './Cocktail'
+export { default as Newsletter } from './Newsletter'
+export { default as HomeLayout } from './HomeLayout'
+export { default as Error } from './Error'
