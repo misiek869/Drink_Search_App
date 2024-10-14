e7988e137b45418530ca21141b66cd75f5607c30
Michal Gawik <gawlik.zator@o2.pl>
2024-10-14 06:32:30 +0200

    add css

---
+++ src/assets/wrappers/AboutPageWrapper.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+	p {
+		line-height: 2;
+		color: var(--grey-600);
+		margin-top: 2rem;
+	}
+`
+
+export default Wrapper
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,18 @@
-import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/AboutPageWrapper'
 
 const About = () => {
 	return (
-		<div>
-			<h1>About</h1>
-			<Link to='/'>Home</Link>
-		</div>
+		<Wrapper>
+			<h3>About Us</h3>
+			<p>
+				Introducing "DrinkSearcher" – the ultimate party companion app that taps
+				into the amusing Cocktails DB API to bring you delightful cocktail
+				recipes. With just a tap of your finger, you'll unveil a treasure trove
+				of enchanting drink ideas that will make your taste buds dance and your
+				friends leap with joy. Get ready to elevate your mixology skills, one
+				fabulous mocktail at a time, and let the laughter and fun flow freely!
+			</p>
+		</Wrapper>
 	)
 }
 
