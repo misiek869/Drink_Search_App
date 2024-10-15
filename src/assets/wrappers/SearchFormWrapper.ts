95e0229cbf716eb8b38c62697accfd6d6e1fa8f2
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 13:47:30 +0200

    style form

--- src/assets/wrappers/SearchFormWrapper.ts
+++ src/assets/wrappers/SearchFormWrapper.ts
@@ -1,5 +1,22 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.article``
+const Wrapper = styled.div`
+	margin-bottom: 4rem;
+
+	.form {
+		display: grid;
+		grid-template-columns: 1fr auto;
+	}
+
+	.form-input {
+		border-bottom-right-radius: 0;
+		border-top-right-radius: 0;
+	}
+
+	.btn {
+		border-bottom-left-radius: 0;
+		border-top-left-radius: 0;
+	}
+`
 
 export default Wrapper
