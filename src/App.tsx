a6b2806315af22d57c4c309e368f2b32310ff4c4
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 14:05:15 +0200

    setup react query to cache drinks

--- src/App.tsx
+++ src/App.tsx
@@ -63,3 +63,17 @@ function App() {
 }
 
 export default App
+
+// const searchCocktailsQuery = (searchTerm) => {
+// 	return {
+// 		queryKey: ['search', searchTerm || 'all'],
+// 		queryFn: async () => {
+// 			// UPDATE !!!
+// 			// Default to 'a' if no search term is provided since API has changed
+// 			searchTerm = searchTerm || 'a';
+
+// 			const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+// 			return response.data.drinks;
+// 		},
+// 	};
+// };
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,19 +2,29 @@ import { useLoaderData } from 'react-router-dom'
 import axios from 'axios'
 import CocktailList from '../components/CocktailList'
 import SearchForm from '../components/SearchForm'
+import { useQuery } from '@tanstack/react-query'
 
 const drinkApi = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+
+const searchCocktailQuery = searchTerm => {
+	return {
+		queryKey: ['search', searchTerm || 'all'],
+		queryFn: async () => {
+			const response = await axios.get(`${drinkApi}${searchTerm}`)
+			return response.data.drinks
+		},
+	}
+}
 
 export const loader = async ({ request }) => {
 	const url = new URL(request.url)
 	const searchTerm = url.searchParams.get('search') || 'vodka'
-	const response = await axios.get(`${drinkApi}${searchTerm}`)
-	return { drinks: response.data.drinks, searchTerm }
+	return { searchTerm }
 }
 
 const Landing = () => {
-	const { drinks, searchTerm } = useLoaderData()
-	console.log(drinks)
+	const { searchTerm } = useLoaderData()
+	const { data: drinks } = useQuery(searchCocktailQuery(searchTerm))
 
 	return (
 		<>
