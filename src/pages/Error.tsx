9955a70b4a79f5cacb5c6a8877c596fbb93df2a6
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 05:31:49 +0200

    create error handling logic

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import Wrapper from '../assets/wrappers/ErrorWrapper'
+import { Link, useRouteError } from 'react-router-dom'
+import img from '../assets/undraw_server_down_s-4-lk.svg'
+
+type RouteError = {
+	status: number
+	message?: string
+}
 
 const Error = () => {
-	return <div></div>
+	const error = useRouteError() as RouteError
+
+	if (error.status === 404) {
+		return (
+			<Wrapper>
+				<div>
+					<img src={img} alt='404 error image' />
+					<h4>Upss</h4>
+					<p>Something went wrong</p>
+					<Link to={'/'}>Back Home</Link>
+				</div>
+			</Wrapper>
+		)
+	}
+	return (
+		<Wrapper>
+			<div>
+				<h4>Something went wrong</h4>
+			</div>
+		</Wrapper>
+	)
 }
 
 export default Error
