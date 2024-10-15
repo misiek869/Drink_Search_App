0a5fed4e9fdde6c9c662abb34a22d1b58e7e54c6
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 09:03:10 +0200

    creat file

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import 
 
-const CocktailCard = () => {
+type CocktailCardProps = {
+	image: string
+	name: string
+	id: string
+	glass: string
+}
+
+const CocktailCard = ({ image, name, id, glass }: CocktailCardProps) => {
 	return <div>Card</div>
 }
 
