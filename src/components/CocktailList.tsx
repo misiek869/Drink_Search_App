e51117fa537ec4367cf928a18f08b725478110ed
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 08:52:47 +0200

    add type

--- src/components/CocktailList.tsx
+++ src/components/CocktailList.tsx
@@ -1,17 +1,37 @@
 import React from 'react'
 
+type Drink = {
+	idDrink: number
+	strDrink: string
+	strDrinkThumb: string
+	strAlcoholic: string
+	strGlass: string
+}
+
 type CocktailListProps = {
-	drinks: []
+	drinks: Drink[]
 }
 
 const CocktailList = ({ drinks }: CocktailListProps) => {
-	if (!drinks) {
+	if (!drinks || drinks.length === 0) {
 		return (
 			<h4 style={{ textAlign: 'center' }}>
 				We don't have the drink you're looking for
 			</h4>
 		)
 	}
+
+	const formattedDrinks = drinks.map(item => {
+		const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item
+
+		return {
+			id: idDrink,
+			name: strDrink,
+			image: strDrinkThumb,
+			info: strAlcoholic,
+			glass: strGlass,
+		}
+	})
 
 	return <div></div>
 }
