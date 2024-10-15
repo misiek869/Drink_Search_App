fe73b498b8fb7f298437b93c521a940305c6cc4c
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 11:36:45 +0200

    create drink layout

--- src/pages/Cocktail.tsx
+++ src/pages/Cocktail.tsx
@@ -1,6 +1,6 @@
 import { useLoaderData, Link } from 'react-router-dom'
 import axios from 'axios'
-// import Wrapper from '../assets/wrappers/CocktailPageWrapper'
+import Wrapper from '../assets/wrappers/CocktailPageWrapper'
 import { Drink } from '../components/CocktailList'
 
 const singleCocktailUrl =
@@ -30,7 +30,36 @@ const Cocktail = () => {
 		strInstructions: instructions,
 	} = singleDrink
 
-	return <div></div>
+	return (
+		<Wrapper>
+			<header>
+				<Link to={'/'}>back home</Link>
+				<h3>{name}</h3>
+			</header>
+
+			<div className='drink'>
+				<img className='img' src={image} alt={name} />
+				<div className='drink-info'>
+					<p>
+						<span className='drink-data'>category: </span>
+						{category}
+					</p>
+					<p>
+						<span className='drink-data'>info: </span>
+						{info}
+					</p>
+					<p>
+						<span className='drink-data'>glass: </span>
+						{glass}
+					</p>
+					<p>
+						<span className='drink-data'>instructions: </span>
+						{instructions}
+					</p>
+				</div>
+			</div>
+		</Wrapper>
+	)
 }
 
 export default Cocktail
