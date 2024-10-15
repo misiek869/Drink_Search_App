482293fc7ce3379d8b3ca17a80dfe4de79e30cd8
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 09:11:25 +0200

    build cocktail card

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,15 +1,30 @@
 import { Link } from 'react-router-dom'
-import 
+import Wrapper from '../assets/wrappers/CocktailCardWrapper'
 
 type CocktailCardProps = {
 	image: string
 	name: string
-	id: string
+	id: number
 	glass: string
+	info: string
 }
 
-const CocktailCard = ({ image, name, id, glass }: CocktailCardProps) => {
-	return <div>Card</div>
+const CocktailCard = ({ image, name, id, info, glass }: CocktailCardProps) => {
+	return (
+		<Wrapper>
+			<div className='img-container'>
+				<img src={image} alt={'name'} className='img' />
+				<div className='footer'>
+					<h4>{name}</h4>
+					<h5>{glass}</h5>
+					<p>{info}</p>
+					<Link className='btn' to={`/cocktail/${id}`}>
+						See More
+					</Link>
+				</div>
+			</div>
+		</Wrapper>
+	)
 }
 
 export default CocktailCard
