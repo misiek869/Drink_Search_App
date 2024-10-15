c272d80c9f6b546fb1544a39e4f7716c5b8fb3d3
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 09:37:10 +0200

    create styles

--- src/assets/wrappers/CocktailCardWrapper.ts
+++ src/assets/wrappers/CocktailCardWrapper.ts
@@ -1,5 +1,41 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.div``
+const Wrapper = styled.article`
+	background: var(--grey-100);
+	box-shadow: var(--shadow-2);
+	transition: var(--transition);
+	display: grid;
+	grid-template-rows: auto 1fr;
+	border-radius: var(--borderRadius);
+
+	&:hover {
+		box-shadow: var(--shadow-4);
+		background: var(--grey-100);
+	}
+
+	img {
+		height: 15rem;
+		border-top-right-radius: var(--borderRadius);
+		border-top-left-radius: var(--borderRadius);
+	}
+
+	.footer {
+		padding: 1.5rem;
+
+		h4,
+		h5 {
+			margin-bottom: 0.5rem;
+		}
+		h4 {
+			font-weight: 600;
+		}
+		p {
+			margin-bottom: 1rem;
+		}
+		p {
+			color: var(--grey-400);
+		}
+	}
+`
 
 export default Wrapper
