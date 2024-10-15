6f8a9a46e5eef676225e0c079e1f03a6071e6d92
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 12:47:20 +0200

    add react taostify

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<ToastContainer position='top-left' autoClose={2000} />
+		<App />
+	</StrictMode>
 )
