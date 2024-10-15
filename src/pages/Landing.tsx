57db017ca7ca773f439401d1168a4c4577f8daa0
Michal Gawik <gawlik.zator@o2.pl>
2024-10-15 14:12:01 +0200

    setup react query to cache drinks

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -16,11 +16,14 @@ const searchCocktailQuery = searchTerm => {
 	}
 }
 
-export const loader = async ({ request }) => {
-	const url = new URL(request.url)
-	const searchTerm = url.searchParams.get('search') || 'vodka'
-	return { searchTerm }
-}
+export const loader =
+	queryClient =>
+	async ({ request }) => {
+		const url = new URL(request.url)
+		const searchTerm = url.searchParams.get('search') || 'vodka'
+		await queryClient.ensureQueryData(searchCocktailQuery(searchTerm))
+		return { searchTerm }
+	}
 
 const Landing = () => {
 	const { searchTerm } = useLoaderData()
