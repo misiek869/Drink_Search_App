abe8bf896f5eb224100e7797f77fce018424b406
Michal Gawik <gawlik.zator@o2.pl>
2024-10-17 13:33:08 +0200

    add logic

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
-
+import { useState } from 'react'
 const ThemeToggle = () => {
+	const [isDarkTheme, setIsDarkTheme] = useState<boolean>()
+
+	const toggleDarkTheme = (): void => {
+		const newDarkTheme = !isDarkTheme
+		setIsDarkTheme(newDarkTheme)
+		localStorage.setItem('darkTheme', String(newDarkTheme))
+	}
+
+	useEffect(() => {
+		document.body.classList.toggle('dark-theme', isDarkTheme)
+	}, [isDarkTheme])
+
 	return <div></div>
 }
 
