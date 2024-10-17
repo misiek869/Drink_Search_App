21d8176c915eaa19c17d03d8b59caa4570f71ac4
Michal Gawik <gawlik.zator@o2.pl>
2024-10-17 13:35:24 +0200

    add logic

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { IoMoon, IoSunnyOutline } from 'react-icons/io5'
+
 const ThemeToggle = () => {
 	const [isDarkTheme, setIsDarkTheme] = useState<boolean>()
 
@@ -12,7 +14,15 @@ const ThemeToggle = () => {
 		document.body.classList.toggle('dark-theme', isDarkTheme)
 	}, [isDarkTheme])
 
-	return <div></div>
+	return (
+		<button className='theme-btn' onClick={toggleDarkTheme} type='button'>
+			{isDarkTheme ? (
+				<IoSunnyOutline className='theme-icon' />
+			) : (
+				<IoMoon className='theme-icon' />
+			)}
+		</button>
+	)
 }
 
 export default ThemeToggle
