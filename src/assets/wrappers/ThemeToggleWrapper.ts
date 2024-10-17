b115a30474d434a8e38b20c0be8ca285ed2e8afa
Michal Gawik <gawlik.zator@o2.pl>
2024-10-17 13:41:07 +0200

    create wrapper

---
+++ src/assets/wrappers/ThemeToggleWrapper.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.button`
+	.theme-btn {
+		position: absolute;
+		background: transparent;
+		border-color: transparent;
+		padding: 0 1rem;
+		left: 0;
+		height: 2rem;
+		display: grid;
+		place-items: center;
+		cursor: pointer;
+	}
+
+	.theme-icon {
+		font-size: 1.25rem;
+		color: var(--textColor);
+		transition: all 0.3s linear;
+	}
+
+	.theme-icon:hover {
+		transform: scale(1.3);
+	}
+`
+
+export default Wrapper
