d5d0064b0e72de92e16931bb5bee9994202b7bac
Michal Gawik <gawlik.zator@o2.pl>
2024-10-18 06:04:05 +0200

    change background

--- src/assets/wrappers/AboutPageWrapper.ts
+++ src/assets/wrappers/AboutPageWrapper.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
+	background: var(--grey-200);
+	padding: 4rem 4rem 6rem;
+	border: 1px solid var(--grey-300);
+	border-radius: var(--borderRadius);
+	box-shadow: var(--shadow-3);
 	p {
 		line-height: 2;
 		color: var(--grey-600);
-		margin-top: 2rem;
+		margin-top: 3rem;
 	}
 `
 
